d79de8121a6531c4a45c40dfdefc64bcc9c39d67
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2021-03-08 12:53:34 +0000

    Add files via upload

---
+++ SPIN (VB)/SPIN (VB)/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.btnSpin = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 77)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(130, 63)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Location = New System.Drawing.Point(148, 77)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(130, 63)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox3.Location = New System.Drawing.Point(284, 77)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(130, 63)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'btnSpin
+        '
+        Me.btnSpin.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpin.Location = New System.Drawing.Point(176, 172)
+        Me.btnSpin.Name = "btnSpin"
+        Me.btnSpin.Size = New System.Drawing.Size(75, 23)
+        Me.btnSpin.TabIndex = 3
+        Me.btnSpin.Text = "SPIN"
+        Me.btnSpin.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MV Boli", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(118, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(191, 34)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Let's Start!!"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(428, 222)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSpin)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Game - SPIN"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents btnSpin As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ SPIN (VB)/SPIN (VB)/Form1.vb
@@ -0,0 +1,79 @@
+﻿'Link do Video -> https://www.youtube.com/watch?v=UQKhHKNmp8g&t=1054s
+
+Public Class Form1
+    Dim m, a, b, c As Integer
+
+    'Timer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        m = m + 10
+        If m < 1000 Then
+            a = Int(1 + Rnd() * 5)
+            b = Int(1 + Rnd() * 5)
+            c = Int(1 + Rnd() * 5)
+
+            'Enquanto as imagens estiverem a passar o botão do SPIN vai
+            'ficar "..."
+            btnSpin.Text = "..."
+
+            Select Case a
+                Case 1
+                    PictureBox1.Image = My.Resources.gato
+                Case 2
+                    PictureBox1.Image = My.Resources.leao
+                Case 3
+                    PictureBox1.Image = My.Resources.pantera
+                Case 4
+                    PictureBox1.Image = My.Resources.tigre
+                Case 5
+                    PictureBox1.Image = My.Resources.gatoselvagem
+            End Select
+
+            Select Case b
+                Case 1
+                    PictureBox2.Image = My.Resources.gato
+                Case 2
+                    PictureBox2.Image = My.Resources.leao
+                Case 3
+                    PictureBox2.Image = My.Resources.pantera
+                Case 4
+                    PictureBox2.Image = My.Resources.tigre
+                Case 5
+                    PictureBox2.Image = My.Resources.gatoselvagem
+            End Select
+
+            Select Case c
+                Case 1
+                    PictureBox3.Image = My.Resources.gato
+                Case 2
+                    PictureBox3.Image = My.Resources.leao
+                Case 3
+                    PictureBox3.Image = My.Resources.pantera
+                Case 4
+                    PictureBox3.Image = My.Resources.tigre
+                Case 5
+                    PictureBox3.Image = My.Resources.gatoselvagem
+            End Select
+        Else
+            Timer1.Enabled = False
+            'Button do SPIN volta ao normal
+            btnSpin.Text = "SPIN"
+            m = 0
+            If a = b Then
+                Label1.Text = "Fraquinho! 100€"
+            ElseIf a = c Then
+                Label1.Text = "Nada mal! 200€"
+            ElseIf b = c Then
+                Label1.Text = "Boa! 300€"
+            ElseIf a = b And b = c Then
+                Label1.Text = "JACKPOT!! 1000€"
+            Else
+                Label1.Text = "Tenta de novo! 0€"
+            End If
+        End If
+    End Sub
+
+    'Button Spin
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSpin.Click
+        Timer1.Enabled = True
+    End Sub
+End Class
